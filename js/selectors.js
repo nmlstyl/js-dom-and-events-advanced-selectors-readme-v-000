42846988e2c5b49556a03e1ba30d5ba7e43f4cfa
joey michalina mariano <joey.michalina.mariano@gmail.com>
2018-06-20 19:20:01 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,32 @@
 // declare your functions here...
+function paragraphSelector(){
+  let result
+  result = $('p')
+  // returns and array
+  return result
+}
+
+function lastImageSelector(){
+  let result
+  result = $('img:last')[0]
+  return result
+}
+
+function ninjaBabySelector(){
+  let result
+  result = $('#baby-ninja')[0]
+  return result
+}
+
+function  divSelector(){
+  let result
+  result = $('.pics')
+  // returns and array
+  return result
+}
+
+function firstListItem(){
+  let result
+  result = $('#pic-list li:first')[0]
+  return result
+}
